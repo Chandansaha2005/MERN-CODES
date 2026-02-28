fa5d2a834b858001216a7332b18b932ce0209cb4
Chandan Saha <chandansaha1945@gmail.com>
2026-02-28 20:04:05 +0530

    update process.js

--- 0- NODE BASICS/process.js
+++ 0- NODE BASICS/process.js
@@ -1,12 +1,12 @@
 // process : This object provides information about,
-// and control over, the current Node.js process.
+// and control over, the current  .
 
 // process.argv : returns an array containing the 
 // command-line arguments passed when the Node.js
 // process was launched.
 
 let print = console.log;
-print(process.argv) 
+print(process.argv)  
 
 // node process.js
 // [
@@ -24,3 +24,16 @@ print(process.argv)
 //   'a',
 //   'boy'
 // ]
+
+let args = process.argv
+for(let i = 0 ; i <args.length ; i++){
+    print("hello",args[i])
+}
+
+// hello C:\Program Files\nodejs\node.exe
+// hello F:\WEB DEVELOPMENT\MERN\BACKEND LESSONS\NODE-BACKEND-TUTORIAL\0- NODE BASICS\process.js
+// hello chandan
+// hello saha
+// hello is
+// hello a
+// hello boy
