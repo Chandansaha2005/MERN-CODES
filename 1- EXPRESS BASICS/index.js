b34752dd5d4b857f263548dc9d28da12d7496f29
Chandan Saha <chandansaha1945@gmail.com>
2026-03-13 21:38:32 +0530

    routes

--- 1- EXPRESS BASICS/index.js
+++ 1- EXPRESS BASICS/index.js
@@ -4,18 +4,38 @@ const app = express()// now "app" is a Server instence
 //print(app) this will print all in-build fuctions of express module 
 let port = 3000; //Ports are the logical endpoints of a network connection that is used to exchange information between a web server and a web client 
 
-app.listen(port,()=> {
+app.listen(port, () => {
     print(`app is listening on the port ${port}`)
 })
 
-app.use((req,res)=>{
-    // print("request received")
-    // res.send("Response send"); 
-    res.send({
-        name: "chandan",
-        number: 123
-    }); 
-    // res.send("<h1>This is an example of Html Response</h1>"); 
+// app.use((req,res)=>{
+//     // print("request received")
+//     // res.send("Response send"); 
+//     res.send({
+//         name: "chandan",
+//         number: 123
+//     }); 
+//     // res.send("<h1>This is an example of Html Response</h1>"); 
+// })
+
+//routing
+
+app.get("/", (req, res) => {
+    res.send("you connected with root path")
 })
+app.get("/route1", (req, res) => {
+    res.send("you connected with route1")
+})
+app.get("/route2", (req, res) => {
+    res.send("you connected with route2")
+})
+// app.get('*', (req, res) => {
+//     res.send("Page not found");
+// });
+app.post("/", (req, res) => {
+    res.send("you connected with root using post")
+})
+
+
 
 
