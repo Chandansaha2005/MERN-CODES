8f09ff53822a05a4964fbca4dc0eeb8217379e54
Chandan Saha <chandansaha1945@gmail.com>
2026-07-11 00:51:16 +0530

    done

--- REACT_JS/1- EXPORT IMPORT/script.js
+++ REACT_JS/1- EXPORT IMPORT/script.js
@@ -3,8 +3,9 @@ console.log(a) // not possible as the variable is in another file
 import a from './test1.js'
 console.log(a) // will print 10 
 //Default export from default.js
-
-
+import pokemon from './default.js' 
+console.log("who's this pokemon?!!!!!!\nit's",pokemon)
+//as it is default export we can name it anything while importing
 
 
 //Named export from named.js
